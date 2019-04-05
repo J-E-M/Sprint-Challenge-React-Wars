b4fd51279b39637dc59d2d574622b42d45e34af8
J-E-M <javonmccoy22@gmail.com>
2019-04-05 15:47:24 -0500

    added more stuff in card

--- starwars/src/components/Cards.js
+++ starwars/src/components/Cards.js
@@ -4,7 +4,9 @@ const Cards = props => {
   console.log(props.cardsProp);
   return(
       <div className="card">
-        <h3>{props.cardsProp.name}</h3>
+        <h3>Name:{props.cardsProp.name}</h3>
+        <h3>Height:{props.cardsProp.height}</h3>
+        <h3>Mass:{props.cardsProp.mass}</h3>
       </div>
   )
 }
